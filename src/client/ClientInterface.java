497b8386f9e98bfe21822a0571fd859b022ed41a
UshaPraveena <ushapraveena.b@gmail.com>
2016-04-22 17:23:15 -0700

    Initial version 1

---
+++ src/client/ClientInterface.java
@@ -0,0 +1,9 @@
+package client;
+
+import server.Client2Server;
+import server.Server2Client;
+
+public interface ClientInterface {
+	public void setEnvironment(Server2Client a);
+        public Server2Client sendclientanswer(Client2Server clientanswer);
+}
